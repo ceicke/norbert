2a8ac8b90f92436b77a481a11e5184b4f4ad03da
Christoph Eicke <eicke@yfu.de>
2017-11-25 16:40:26 +0100

    use ruby-nfc to communicate with the NFC cards

--- norbert.rb
+++ norbert.rb
@@ -1,32 +1,26 @@
-require 'nfc'
 require 'ruby-mpd'
 require 'json'
 require 'rpi_gpio'
-
+require 'ruby-nfc'
 require 'pry'
 
-ctx = NFC::Context.new
-dev = ctx.open nil
 
 $mpd = MPD.new
 $mpd.connect
 
-file = File.read('/root/norbert-database.json')
-database = JSON.parse(file)
-
 statusLED = 12
-stopButton = 26
-nextButton = 16
-previousButton = 13
-volumeUpButton = 20
-volumeDownButton = 19
+$stopButton = 26
+$nextButton = 16
+$previousButton = 13
+$volumeUpButton = 20
+$volumeDownButton = 19
 
 RPi::GPIO.set_numbering :bcm
 
 RPi::GPIO.setup statusLED, :as => :output
 RPi::GPIO.set_high statusLED
 
-controlButtons = [stopButton, nextButton, previousButton, volumeUpButton, volumeDownButton]
+controlButtons = [$stopButton, $nextButton, $previousButton, $volumeUpButton, $volumeDownButton]
 
 RPi::GPIO.setup statusLED, :as => :output
 RPi::GPIO.set_high statusLED
@@ -35,7 +29,7 @@ controlButtons.each do |controlButton|
   RPi::GPIO.setup controlButton, :as => :input, :pull => :up
 end
 
-def toggle_pause
+def toggle_pause 
   $mpd.pause=($pause_state)
   $pause_state = !$pause_state
 end
@@ -45,12 +39,12 @@ def set_volume(volume)
 end
 
 def increase_volume
-  if Time.now.hour <= 7
-    max_volume = 94
+  if Time.now.hour < 7
+    max_volume = 90
   else
     max_volume = 100
   end
-  if $current_volume <= (max_volume - 2)
+  if $current_volume <= max_volume
     $current_volume += 2
     set_volume($current_volume)
   end
@@ -65,28 +59,44 @@ end
 
 $currently_playing_uuid = nil
 $pause_state = true
-$current_volume = 70
+$current_volume = 80
 set_volume($current_volume)
 $mpd.stop
 $mpd.clear
 
-loop do
-  card_uuid = dev.poll.to_s
-  if card_uuid != '-90'
-    database.each do |entry|
-      if entry['card_uuid'] == card_uuid && card_uuid != $currently_playing_uuid
-        $currently_playing_uuid = card_uuid
-        $mpd.stop
-        $mpd.clear
-        $mpd.where({album: entry['album']}, {add: true})
-        $mpd.play
-      end
-      "Card: #{card_uuid}"
+
+def card_control
+
+file = File.read('/root/norbert-database.json')
+database = JSON.parse(file)
+
+readers = NFC::Reader.all
+readers[0].poll(IsoDep::Tag, Mifare::Classic::Tag, Mifare::Ultralight::Tag) do |tag|
+  card_uuid = tag.uid_hex.upcase
+  database.each do |entry|
+    if entry['card_uuid'] == card_uuid && (card_uuid != $currently_playing_uuid or $mpd.stopped?)
+      $currently_playing_uuid = card_uuid
+      $mpd.stop
+      $mpd.clear
+      $mpd.where({album: entry['album']}, {add: true})
+      $mpd.play
     end
   end
-  toggle_pause if RPi::GPIO.low? stopButton
-  $mpd.next if RPi::GPIO.low? nextButton
-  $mpd.previous if RPi::GPIO.low? previousButton
-  increase_volume() if RPi::GPIO.low? volumeUpButton
-  decrease_volume() if RPi::GPIO.low? volumeDownButton
 end
+end
+
+def button_control
+while(true) 
+  toggle_pause if RPi::GPIO.low? $stopButton
+  $mpd.next if RPi::GPIO.low? $nextButton
+  $mpd.previous if RPi::GPIO.low? $previousButton
+  increase_volume() if RPi::GPIO.low? $volumeUpButton
+  decrease_volume() if RPi::GPIO.low? $volumeDownButton
+  sleep(0.2)
+end
+end
+
+button_thread = Thread.new{button_control()}
+card_thread = Thread.new{card_control()}
+button_thread.join
+card_thread.join
